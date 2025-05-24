9fe32a48b5a3c3bee1a08d015b48b49667a83556
Thainan Luiz <thainandj@hotmail.com>
2025-05-24 02:48:05 -0300

    Copilot: implementa o comentário sugerido antes de chamar a função run_game

--- src/pages/game/game_page.h
+++ src/pages/game/game_page.h
@@ -6,6 +6,11 @@
 #ifndef GAME_PAGE_H
 #define GAME_PAGE_H
 
+/**
+ * @brief Função que roda o jogo.
+ * @pre O allegro deve estar inicializado e a tela precisa estar visivel.
+ * @post O jogo é rodado e a tela é atualizada a cada n quadros.
+ */
 void run_game();
 
 #endif
